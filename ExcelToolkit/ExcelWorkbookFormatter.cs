e41ab57feb424c3bfeb2a11798ec7220574af15c
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-11-21 10:38:36 -0600

    Create ExcelWorkbookFormatter.cs
    Used to format the workbook before calling the rest of the data.

---
+++ ExcelToolkit/ExcelWorkbookFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelToolkit.DataFormatting;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelToolkit
+{
+    public class ExcelWorkbookFormatter : IExcelData
+    {
+        public void CreateData(_Workbook workbook, int column_offset, int row_offset)
+        {
+            workbook.Worksheets.Delete();
+            workbook.Worksheets.Add();
+            workbook.Worksheets[1].Name = "Data";
+        }
+    }
+}
