0f60ca6bbb246dee5c981f745f34ed588bac4758
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-11-23 14:16:27 -0600

    Create ChartTypesENUM.cs

---
+++ ExcelToolkit/DataFormatting/ChartTypesENUM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToolkit.DataFormatting
+{
+    /// <summary>
+    /// An enum to embody all of the different kinds of charts that Mr. Plotter can create
+    /// </summary>
+    public enum MrPlotterChartTypes { BarGraph = 0 }
+}
